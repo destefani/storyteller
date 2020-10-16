434f64ef74afa147431fd93dec997ee4f5749895
Marcelo Destefani <marcelo.destefani@zalando.de>
2020-10-16 19:55:03 +0200

    basic streamlit app

--- generator.py
+++ generator.py
@@ -1,4 +1,10 @@
 from transformers import pipeline
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('Automated Storyteller')
+
 
 INPUT_TEXT = "As far as I am concerned, I will"
 MAX_LENGHT = 50
@@ -8,3 +14,6 @@ text_generator = pipeline("text-generation")
 generated_text = text_generator(INPUT_TEXT, max_length=MAX_LENGHT, do_sample=False)[0][
     "generated_text"
 ]
+
+st.write(generated_text)
+
