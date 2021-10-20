e91a59a23967596619b32b7f4fad63e10372d037
Marcelo destefani <marcelo@hastapronto.cl>
2021-10-20 22:51:38 +0200

    typo and lenght

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from text_generator import generator
 
 def main():
     st.title('Automated Storyteller')
-    st.write('This is Machine Learning based automatic story generator.\
-            For a story to be generated you have to specify the number of words the story to have,\
-            and write the start of the story. Remember that the longer the story the longer it will takte to be generated')
+    st.write('This is a Machine Learning based automatic story generator.\
+            For a story to be generated you have to specify the number of words the story to have\
+            and write the start of it. Remember that the longer the story the longer it will takte to be generated')
 
     max_lenght = st.selectbox(
         'How long do you want the story to be?',
-        [50, 100]
+        [50, 100, 200, 300]
     )
 
     input_text = st.text_input(
-        label='Write a beginning to the story...',
+        label='Write a beginning to your story...',
     )
 
     # Text generator
